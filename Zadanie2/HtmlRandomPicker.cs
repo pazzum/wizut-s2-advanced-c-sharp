a8f6bae1e0dd52c99caac30810b444adc7662f51
pazzum <mgnyp@wi.zut.edu.pl>
2018-03-23 23:57:30 +0100

    lab 2 - part 1

---
+++ Zadanie2/HtmlRandomPicker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Zadanie2
+{
+    class HtmlRandomPicker
+    {
+        private List<string> htmls = new List<string>();
+
+        public HtmlRandomPicker()
+        {
+            htmls.Add(File.ReadAllText("table1.html"));
+            htmls.Add(File.ReadAllText("table2.html"));
+            htmls.Add(File.ReadAllText("table3.html"));
+            htmls.Add(File.ReadAllText("table4.html"));
+        }
+
+        public string Pick()
+        {
+            return htmls[new Random().Next(htmls.Count)];
+        }
+    }
+}
--- Zadanie2/Server.cs
+++ Zadanie2/Server.cs
@@ -1,43 +1,77 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Zadanie2
 {
     public class Server
     {
         private readonly TcpListener _tcpListener = new TcpListener(IPAddress.Any, 8020);
+        private readonly string _serverName = "advanced-c-sharp";
+        private bool isListening = true;
 
         public Server()
         {
-            while (true)
+            var thread = new Thread(new ThreadStart(ListenForConnections));
+            thread.Start();
+        }
+
+        private void ListenForConnections()
+        {
+            while (isListening)
             {
                 _tcpListener.Start();
                 Console.WriteLine("Server started.");
-                var tcpClient = _tcpListener.AcceptTcpClient();
-                Console.WriteLine("Connected");
+                var tcpSocket = _tcpListener.AcceptSocket();
+                if (tcpSocket.Connected)
+                {
+                    var thread = new Thread(new ThreadStart(delegate () {
+                            Console.WriteLine("Connected." + tcpSocket.AddressFamily);
 
-                var message = Encoding.ASCII.GetBytes("HTTP /1.1 200 OK");
-                
-                var stream = new System.IO.StreamWriter(tcpClient.GetStream());
-                stream.Write("HTTP/1.1 200 OK");
-                stream.Write(Environment.NewLine);
-                stream.Write("Content-Type: text/plain; charset=UTF-8");
-                stream.Write(Environment.NewLine);
-                stream.Write("Content-Length: " + message.Length);
-                stream.Write(Environment.NewLine);
-                stream.Write(Environment.NewLine);
-                stream.Write(message);
-                stream.Flush();
-                tcpClient.Close();
-                _tcpListener.Stop();
-                Console.WriteLine("Message sent.");
+                            var buffer = new Byte[tcpSocket.Available];
+                            tcpSocket.Receive(buffer);
+
+                            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+
+                            var randomPicker = new HtmlRandomPicker();
+                            var body = randomPicker.Pick();
+
+                            var memoryStream = FormatResponseBody(body);
+
+                            tcpSocket.Send(memoryStream.GetBuffer());
+                            tcpSocket.Close();
+
+                            Console.WriteLine("Disconnected.");
+                        }));
+                    thread.Start();
+                }
             }
+            _tcpListener.Stop();
+        }
 
-            //var response = _tcpListener.GetContext().Response;
-                       
-            //response.Close();
+        private MemoryStream FormatResponseBody(string body)
+        {
+            var memoryStream = new MemoryStream();
+            var stream = new StreamWriter(memoryStream);
+            stream.WriteLine("HTTP/1.1 200 OK");
+            stream.WriteLine("Expires: -1");
+            stream.WriteLine("Date: " + DateTime.Now);
+            stream.WriteLine("Access-Control - Allow - Origin : *");
+            stream.WriteLine("Content-Type : text/html; charset=utf-8");
+            stream.WriteLine("Content-Length: " + body.Length);
+            stream.WriteLine(_serverName);
+            stream.WriteLine("x - xss - protection : 1; mode = block");
+            stream.WriteLine("Connection : close");
+            stream.Write(stream.NewLine);
+            stream.WriteLine(body);
+            stream.Flush();
+
+            return memoryStream;
         }
     }
 }
