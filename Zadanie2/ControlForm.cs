6542eeeb99f9697bc71cb80ac7e82445dd5b1bea
pazzum <mgnyp@wi.zut.edu.pl>
2018-03-27 20:56:06 +0200

    lab 2 - part 2

---
+++ Zadanie2/ControlForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zadanie2
+{
+    public partial class ControlForm: Form
+    {
+        private string _portTextBoxValue;
+        private Server _server;
+
+        public ControlForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            try
+            {
+                _server = new Server(Int32.Parse(_portTextBoxValue));
+            }
+            catch(Exception exception)
+            {
+                this.Status.Text = exception.Message;
+                this.Status.ForeColor = Color.Red;
+            }
+
+            this.Status.Text = "Uruchomiono";
+            this.Status.ForeColor = Color.Green;
+
+            button.Enabled = false;
+            this.Stop.Enabled = true;
+        }
+
+        private void PortTextbox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            _portTextBoxValue = textBox.Text;
+        }
+
+        private void PortTextbox_HandleCreated(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            _portTextBoxValue = textBox.Text;
+        }
+
+        private void HostingDirectoryBrowse_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if(this.HostingFolderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                if(_server != null)
+                {
+                    _server._servingPath = this.HostingFolderBrowserDialog.SelectedPath;
+                }
+                this.SelectedPath.Text = this.HostingFolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            _server.Stop();
+
+            this.Status.Text = "Zatrzymano";
+            this.Status.ForeColor = Color.Red;
+
+            button.Enabled = false;
+            this.Start.Enabled = true;
+        }
+
+        private void ControlForm_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _server.Stop();
+        }
+    }
+}
---
+++ Zadanie2/PathParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class PathParser
+    {
+        private string _servingDirectory;
+        private string _file;
+        private string _buffer;
+
+        public PathParser(string servingDirectory, string buffer)
+        {
+            _servingDirectory = servingDirectory;
+            _buffer = buffer;
+        }
+
+        private void ParseGivenPath()
+        {
+            var splitted = _buffer.Split(' ');
+            _file = splitted[1];
+
+            if(_servingDirectory == null)
+            {
+                _servingDirectory = "";
+                _file = _file.TrimStart('/');
+            }
+
+            if(_file == "/" || _file == "")
+            {
+                _file += "index.html";
+            }
+        }
+
+        public string getPath()
+        {
+            ParseGivenPath();
+            return _servingDirectory + _file;
+        }
+    }
+}
